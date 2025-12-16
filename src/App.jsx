f1b8e5a8f0aba3222f77384c789a48491a47bf13
Alan <alan.jego35@gmail.com>
2025-12-16 08:23:27 +0100

    add tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import GlobeComponent from './components/Globe'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
       <div className="globe-container">
           <GlobeComponent />
+          <Analytics />
       </div>
   )
 }
